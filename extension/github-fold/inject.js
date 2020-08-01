2416eeaba2de9dc79d8821776917db71ceb1d250
Ahmad <me@AhmadAwais.com>
2020-08-01 18:25:34 +0500

    👌 IMPROVE: RM Comment

--- extension/github-fold/inject.js
+++ extension/github-fold/inject.js
@@ -1,14 +1,3 @@
-const isMainRepo = !document.location.pathname.includes("tree/");
-const getClassList = document.querySelector(".repository-content").classList;
-isMainRepo && getClassList.toggle("rgh-files-hidden");
-
-// On load.
-// chrome.extension.sendMessage({}, function (response) {
-// 	var readyStateCheckInterval = setInterval(function () {
-// 		if (document.readyState === 'complete') {
-// 			clearInterval(readyStateCheckInterval);
-// 			const isMainRepo = !document.location.pathname.includes('tree/');
-// 			isMainRepo && document.querySelector('.repository-content').classList.toggle('rgh-files-hidden');
-// 		}
-// 	}, 10);
-// });
+const isMainRepo = !document.location.pathname.includes('tree/');
+const getClassList = document.querySelector('.repository-content').classList;
+isMainRepo && getClassList.toggle('rgh-files-hidden');
